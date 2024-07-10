1872dad606a53eb6eac104af4b13296940c3f5c8
cspartalis <spartalisch@gmail.com>
2024-07-10 20:10:50 +0300

    Final hyperparams, I hope

--- src/reproduce_seed_3407.sh
+++ src/reproduce_seed_3407.sh
@@ -9,14 +9,14 @@
 ###########################
 # python train.py --seed 3407 --cudnn slow --dataset cifar-10 --model resnet18 --batch_size 512 --epochs 150 --loss cross_entropy --optimizer sgd --lr 0.1 --momentum 0.9 --weight_decay 5e-4 --is_lr_scheduler true --warmup_epochs 30 --is_early_stop true --patience 50 --is_class_unlearning false --class_to_forget none
 # python retrain.py --registered_model resnet18-cifar-10-3407-original --is_class_unlearning False
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method our --lr 1e-4 --weight_decay 0.01
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method finetune 
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method neggrad
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method amnesiac
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method bad-teacher
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method scrub
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method ssd
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method unsir
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method our --lr 1e-4 --weight_decay 0.01
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method finetune 
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method neggrad
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method amnesiac
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method bad-teacher
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method scrub
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method ssd
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retrained --method unsir
 
 # ###########################
 # # # ResNet-18 cat
@@ -50,14 +50,14 @@ python unlearn.py --epochs 10 --registered_model resnet18-cifar-10-3407-retraine
 # ###########################
 # python train.py --seed 3407 --cudnn slow --dataset cifar-100 --model resnet18 --batch_size 512 --epochs 150 --loss cross_entropy --optimizer sgd --lr 0.1 --momentum 0.9 --weight_decay 5e-4 --is_lr_scheduler true --warmup_epochs 30 --is_early_stop true --patience 50 --is_class_unlearning false 
 # python retrain.py --registered_model resnet18-cifar-100-3407-original --is_class_unlearning False
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method our --lr 1e-4 --weight_decay 0.01
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method finetune 
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method neggrad
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method amnesiac
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method bad-teacher
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method scrub
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method ssd
-python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method unsir
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method our --lr 1e-4 --weight_decay 0.01
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method finetune 
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method neggrad
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method amnesiac
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method bad-teacher
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method scrub
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method ssd
+# python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrained --method unsir
  
 ###########################
 # # ResNet-18 rocket
@@ -90,21 +90,21 @@ python unlearn.py --epochs 10 --registered_model resnet18-cifar-100-3407-retrain
 ###########################
 # python train.py --seed 3407 --cudnn slow --dataset mufac --model resnet18 --batch_size 512 --epochs 150 --loss cross_entropy --optimizer sgd --lr 0.1 --momentum 0.9 --weight_decay 5e-4 --is_lr_scheduler true --warmup_epochs 30 --is_early_stop true --patience 50 --is_class_unlearning false --class_to_forget none
 # python retrain.py --registered_model resnet18-mufac-3407-original --is_class_unlearning False
-python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method our --lr 1e-4 --weight_decay 0.1 
-python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method finetune 
-python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method neggrad
-python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method amnesiac
-python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method bad-teacher
-python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method scrub
-python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method ssd
-python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method unsir
+python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method our --lr 1e-5 --weight_decay 0.1 
+# python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method finetune 
+# python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method neggrad
+# python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method amnesiac
+# python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method bad-teacher
+# python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method scrub
+# python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method ssd
+# python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained --method unsir
 
 ###########################
 # ViT on CIFAR-10
 ###########################
 # python train.py --seed 3407 --cudnn slow --dataset cifar-10 --model vit --batch_size 64 --epochs 30 --loss cross_entropy --optimizer sgd --lr 0.0001 --momentum 0.9 --weight_decay 0.0005 --is_lr_scheduler False --is_early_stop True --patience 10
 # python retrain.py --registered_model vit-cifar-10-3407-original --is_class_unlearning False 
-# python unlearn.py --epochs 3 --registered_model vit-cifar-10-3407-retrained --batch_size 32 --method our --lr 1e-6 --weight_decay 0.001
+python unlearn.py --epochs 3 --registered_model vit-cifar-10-3407-retrained --batch_size 32 --method our --lr 1e-6 --weight_decay 0.01
 # python unlearn.py --epochs 3 --registered_model vit-cifar-10-3407-retrained --batch_size 32 --method finetune 
 # python unlearn.py --epochs 3 --registered_model vit-cifar-10-3407-retrained --batch_size 32 --method neggrad 
 # python unlearn.py --epochs 3 --registered_model vit-cifar-10-3407-retrained --batch_size 32 --method amnesiac 
@@ -144,7 +144,7 @@ python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained -
 ###########################
 # python train.py --seed 3407 --cudnn slow --dataset cifar-100 --model vit --batch_size 64 --epochs 30 --loss cross_entropy --optimizer sgd --lr 0.0001 --momentum 0.9 --weight_decay 0.0005 --is_lr_scheduler False --is_early_stop True --patience 10
 # python retrain.py --registered_model vit-cifar-100-3407-original --is_class_unlearning False 
-# python unlearn.py --epochs 3 --registered_model vit-cifar-100-3407-retrained --batch_size 32 --method our --lr 1e-6 --weight_decay 0.001
+python unlearn.py --epochs 3 --registered_model vit-cifar-100-3407-retrained --batch_size 32 --method our --lr 1e-6 --weight_decay 0.01
 # python unlearn.py --epochs 3 --registered_model vit-cifar-100-3407-retrained --batch_size 32 --method finetune 
 # python unlearn.py --epochs 3 --registered_model vit-cifar-100-3407-retrained --batch_size 32 --method neggrad 
 # python unlearn.py --epochs 3 --registered_model vit-cifar-100-3407-retrained --batch_size 32 --method amnesiac 
@@ -185,7 +185,7 @@ python unlearn.py --epochs 10 --registered_model resnet18-mufac-3407-retrained -
 ###########################
 # python train.py --seed 3407 --cudnn slow --dataset mufac --model vit --batch_size 64 --epochs 30 --loss cross_entropy --optimizer sgd --lr 0.0001 --momentum 0.9 --weight_decay 0.0005 --is_lr_scheduler False --is_early_stop True --patience 10 --is_class_unlearning False
 # python retrain.py --registered_model vit-mufac-3407-original --is_class_unlearning False 
-# python unlearn.py --epochs 3 --registered_model vit-mufac-3407-retrained --batch_size 32 --method our --lr 1e-5 --weight_decay 0.001
+python unlearn.py --epochs 3 --registered_model vit-mufac-3407-retrained --batch_size 32 --method our --lr 1e-5 --weight_decay 0.01
 # python unlearn.py --epochs 3 --registered_model vit-mufac-3407-retrained --batch_size 32 --method finetune 
 # python unlearn.py --epochs 3 --registered_model vit-mufac-3407-retrained --batch_size 32 --method neggrad 
 # python unlearn.py --epochs 3 --registered_model vit-mufac-3407-retrained --batch_size 32 --method amnesiac 
